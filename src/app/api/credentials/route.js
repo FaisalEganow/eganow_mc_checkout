ee097e300eb895ff54cf2e1225eec060aec0f095
leonard adjei <157623061+leonardeganow@users.noreply.github.com>
2024-06-08 23:23:10 +0000

    updated skeleton loader

--- src/app/api/credentials/route.js
+++ src/app/api/credentials/route.js
@@ -31,8 +31,9 @@ export async function POST(request) {
       // console.log(response.data);
       return NextResponse.json(response.data, { status: 200 });
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
-      { error: "Failed to fetch access token" },
+      { error: error },
       { status: 500 }
     );
   }
